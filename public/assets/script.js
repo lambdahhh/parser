60ebf3df6399fe87f7c63a614547a3280ce80d61
pvolkov <p.volkov@ndv.ru>
2022-03-14 10:45:06 +0300

    add fetch request

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,11 @@
-console.log('hello, world');
+document.querySelector('#submit').addEventListener('click', () => {
+    const site = document.querySelector('#site').value;
+    const page = document.querySelector('#page').value;
+
+    const url = `${site}${page}`;
+
+    fetch(url)
+        .then(res => {
+            document.querySelector('#content').innerHTML = res;
+        })
+});
